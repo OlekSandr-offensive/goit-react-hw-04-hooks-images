5cb8e38dc3b93b967286614186a13daf7b98167b
Oleksandr Tarasiuk <oleksandr.tarasyuk92@gmail.com>
2022-04-02 00:17:19 +0200

    add picture

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -43,7 +43,7 @@ export default function App() {
           setError(null);
         }
       })
-      .catch(error => {
+      .catch(() => {
         setError(` No picture with name :  ${imageName}`);
         setStatus(Status.REJECTED);
       })
